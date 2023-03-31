063564a6c5529a8e4123a80d72dc80bb061bd8b6
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2023-03-31 22:03:12 +0000

    Agrego cod de chat gpt.1

--- src/Controlador/controlador.java
+++ src/Controlador/controlador.java
@@ -1,7 +1,12 @@
 package Vista;
 
 import javax.swing.*;
-
+import Vista.*;
 public class controlador {
-
+    vista v;
+    MarcoImagen MI= new MarcoImagen();
+    public mostrarImg(){
+        MI.setVisible();
+    }
+    
 }
--- src/Modelo/modelo.java
+++ src/Modelo/modelo.java
@@ -1,8 +1,52 @@
 package Modelo;
 
-
-
-public class modelo{
+public class modelo {
     Image imagen;
-    int x,y;
+    int x, y;
+    int sizeX, sizeY;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setSizeX(int sizeX) {
+        this.sizeX = sizeX;
+    }
+
+    public int getSizeX() {
+        return sizeX;
+    }
+
+    public void setSizeY(int sizeY) {
+        this.sizeY = sizeY;
+    }
+
+    public int getSizeY() {
+        return sizeY;
+    }
+
+
+    public Modelo() {
+        imagen = new ImageIcon("./src/imgs/messi.jpeg");
+    }
+
+    public ImageIcon getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(ImageIcon imagen) {
+        this.imagen = imagen;
+    }
 }
