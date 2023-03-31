2826d7324b36941e2ff7a873d3e0e1526dae2d17
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2023-03-31 21:29:42 +0000

    Agrego app mvc

---
+++ src/Launcher/main.java
@@ -0,0 +1,14 @@
+package Launcher;
+ 
+import java.awt.*;
+import java.io.*;
+import javax.swing.*;
+import javax.imageio.*;
+ 
+ 
+public class main {
+ 
+ public static void main(String[] args) {
+    
+ }
+}
---
+++ src/Vista/vista.java
@@ -0,0 +1,22 @@
+package Vista;
+
+import javax.swing.*;
+
+public class vista extends JPanel{
+    public void paintComponent(Graphics g){
+  
+        super.paintComponent(g);
+         
+        File miimagen= new File("./src/imgs/messi.png");
+         
+        try{
+            imagen=ImageIO.read(miimagen); //lanza excepción
+        }
+        catch(IOException e){
+         System.out.println("La imagen no se encuentra");
+        }
+         
+        g.drawImage(imagen, 0, 0, null);  
+         
+    }  
+}
